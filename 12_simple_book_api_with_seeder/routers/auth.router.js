d1b6c38b485fee9e723cb09f80e8cf1269e156b5
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2025-06-15 23:16:18 +0800

    Done with Activity #13: Deploy Your Book API to Render with MongoDB Atlas

--- 12_simple_book_api_with_seeder/routers/auth.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-router.post('/signup', authController.signup);
-router.post('/signin', authController.signin);
-
-module.exports = router; 
